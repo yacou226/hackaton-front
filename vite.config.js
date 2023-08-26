6fc975ca96c111db83420368274d3dd268179c53
yacou226 <bamogoyacouba225@gmail.com>
2023-08-26 21:08:30 +0000

    get user data

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   resolve:{
     alias:{
       '@':path.resolve(__dirname, './src/Components'),
-      screens: '/src/Dashboard',
+      screens: '/src/views/',
       components: '/src/components',
       src: '/src',
     }
