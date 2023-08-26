006b49111a791a273a5711458cf8b19367974a35
yacou226 <bamogoyacouba225@gmail.com>
2023-08-26 22:28:46 +0000

    pushed

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,14 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // server: {
+  //   proxy: {
+  //     '/posts': 'http://localhost:5000/api/v1'
+  //   }
+  // },
   resolve:{
     alias:{
-      '@':path.resolve(__dirname, './src/Components'),
-      screens: '/src/views/',
-      components: '/src/components',
-      src: '/src',
+      '@':path.resolve(__dirname, './src')
     }
   }
 })
