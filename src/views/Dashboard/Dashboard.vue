1fed78c8eed7b60d002c78586d8d103c8e782f36
yacou226 <bamogoyacouba225@gmail.com>
2023-08-27 15:13:46 +0000

    fdfdfdfffdffddfdfdfdffdfdfdfdfd

--- src/views/Dashboard/Dashboard.vue
+++ src/views/Dashboard/Dashboard.vue
@@ -11,24 +11,25 @@ import NavBar from "@/components/NavBar.vue";
 
 	<!-- main div -->
 	<div class="w-screen h-screen ">
-		<div class="bg-gray-50 h-full w-full border flex    p-6  ">
-		<!-- Sidebar -->
+		<!-- <div class="bg-gray-50 h-full w-full border flex    p-6  ">
+		
 		<div class="h-full w-80 bg-white shadow rounded-lg ">
 			<Sidebar />
 		</div>
-		<!-- NavBar and viewRouter -->
+		
 		<div class="w-full h-full ml-4  overflow-y-auto  ">
-			<!-- Navbar -->
+			
 			<div class="bg-white shadow-md w-full h-16 rounded-lg mb   ">
 				<NavBar class="w-full" />
 			</div>
-			<!-- RouterView -->
+			
 			<div class="">
 				<router-view> </router-view>
 			</div>
 
 		</div>
-	</div>
+	</div> -->
+	<h1>helloword</h1>
 	</div>
 </template>
 <style>
